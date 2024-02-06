82641a893bbebffc262068722bec8c6b184f6d3e
Varshini-Shetty <78336214+Varshini-Shetty@users.noreply.github.com>
2024-02-06 09:03:19 +0530

    property added

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,4 +1,4 @@
 export default {
-  //BASE_URL: 'http://103.212.120.217:8000' //test server url
-  BASE_URL: 'https://dfde-103-89-234-140.ngrok-free.app' //ngrok admin url
+  BASE_URL: 'http://103.212.120.217:7080' //test server url
+  //BASE_URL: 'https://dfde-103-89-234-140.ngrok-free.app' //ngrok admin url
 }
